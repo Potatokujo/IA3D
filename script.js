d682b923c206b74a8ac276eb8abbcf7a56c49cae
Potatokujo <110933099+Potatokujo@users.noreply.github.com>
2024-08-23 07:36:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [//abre uma lista qualquer
-    {//abre o objetivo da lista (cada item)
-        enunciado:"Pergunta 1",
-        alternativas:[
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    }
+]
 
-    }//fechar a o objeto da lista (fim do item)
-]//fechando a lista toda (completa)
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    
+}
